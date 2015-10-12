75c663e81223a9d499dd41cbe2d09b3361c0b38c
peryaudo <peryaudo@gmail.com>
2015-10-12 11:54:24 +0900

    Added comments

--- winny/servent.go
+++ winny/servent.go
@@ -6,7 +6,7 @@ import (
 	"log"
 )
 
-// A Servent is an Winny servent.
+// An Winny servent.
 // You should specify Speed and Port while others are optional.
 type Servent struct {
 	Speed    int
@@ -19,7 +19,7 @@ type Servent struct {
 	queryMgr *queryMgr
 }
 
-// ListenAndServe starts Winny servent.
+// Starts Winny servent.
 // It listens on the specified port while trying connecting other nodes.
 // You can add other nodes explicitly by using AddNode().
 func (s *Servent) ListenAndServe() (err error) {
@@ -35,10 +35,13 @@ func (s *Servent) ListenAndServe() (err error) {
 		return
 	}
 
+	// Start node and query managers in other goroutines
 	go s.nodeMgr.ListenAndServe()
 	go s.queryMgr.ListenAndServe()
 
+	// Dispatches a received command to corresponding modules
 	for recvCmd := range s.recvCmd {
+		// true if the received command indicates disconnection request
 		closeConn := false
 
 		switch cmd := recvCmd.cmd.(type) {
@@ -47,6 +50,7 @@ func (s *Servent) ListenAndServe() (err error) {
 		case *cmdQuery:
 			s.queryMgr.RecvQuery <- recvCmd
 
+			// All the commands below indicates disconnection request
 		case *cmdClose:
 			closeConn = true
 		case *cmdCloseTransLimit:
@@ -65,6 +69,7 @@ func (s *Servent) ListenAndServe() (err error) {
 		}
 
 		if closeConn {
+			// Request node manager to disconnect the sender node
 			s.nodeMgr.Disconnect <- recvCmd.From
 		}
 	}
@@ -82,13 +87,14 @@ func (s *Servent) init() {
 	s.queryMgr = newQueryMgr(s)
 }
 
-// AddNode adds other Winny nodes to the node list.
+// Adds other Winny nodes to the node list.
 // The node string must be in the encrypted form (e.g. @fc259bdf....).
 // Nodes with the private IP addresses are ignored.
 // There's no guarantee that the servent will connect to the given nodes.
 func (s *Servent) AddNode(node string) {
 	s.init()
 
+	// The function is unblocking because there's no guarantee that the servent is already started.
 	go func() {
 		s.nodeMgr.AddNodeStr <- node
 	}()
@@ -99,8 +105,9 @@ type queryReq struct {
 	Results chan *FileKey
 }
 
-// Search returns a channel that sends the file keys that matches the keyword.
+// Returns a channel that sends the file keys that match the keyword.
 // If the keyword is an empty string, the channel streams all the file keys.
+// Sending something to the quit channel stops the result stream.
 func (s *Servent) Search(keyword string) (results <-chan *FileKey, quit chan<- struct{}) {
 	s.init()
 
@@ -108,6 +115,7 @@ func (s *Servent) Search(keyword string) (results <-chan *FileKey, quit chan<- s
 		Keyword: keyword,
 		Results: make(chan *FileKey)}
 
+	// The function is unblocking because there's no guarantee that the servent is already started.
 	go func() {
 		s.queryMgr.AddQuery <- q
 	}()
@@ -115,6 +123,7 @@ func (s *Servent) Search(keyword string) (results <-chan *FileKey, quit chan<- s
 	qu := make(chan struct{})
 
 	go func() {
+		// Wait until quit channel receives something and remove the query
 		<-qu
 		s.queryMgr.RemoveQuery <- q.Results
 	}()
@@ -124,18 +133,21 @@ func (s *Servent) Search(keyword string) (results <-chan *FileKey, quit chan<- s
 	return
 }
 
-// KeywordStream returns a channel that streams all the searching keywords.
+// Returns a channel that streams all the searching keywords flowing through the network.
+// Sending something to the quit channel stops the result stream.
 func (s *Servent) KeywordStream() (keywords <-chan string, quit chan<- struct{}) {
 	s.init()
 
 	k := make(chan string)
 	qu := make(chan struct{})
 
+	// The function is unblocking because there's no guarantee that the servent is already started.
 	go func() {
 		s.queryMgr.AddKeywordStream <- k
 	}()
 
 	go func() {
+		// Wait until quit channel receives something and remove the keyword stream
 		<-qu
 		s.queryMgr.RemoveKeywordStream <- k
 	}()
@@ -145,7 +157,7 @@ func (s *Servent) KeywordStream() (keywords <-chan string, quit chan<- struct{})
 	return
 }
 
-// NodeList returns the complete node list the servent has.
+// Returns the complete node list the servent has.
 // The returned strings are in the encrypted form.
 func (s *Servent) NodeList() []string {
 	ch := make(chan []string)
@@ -153,6 +165,8 @@ func (s *Servent) NodeList() []string {
 	return <-ch
 }
 
+// Returns the number of connected nodes.
+// Used by the query manager to adjust request intervals.
 func (s *Servent) connNodeCnt() int {
 	ch := make(chan int)
 	s.nodeMgr.getConnNodeCnt <- ch
